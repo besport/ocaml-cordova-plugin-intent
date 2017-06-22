e98c6a371f58f40c77de84831d31276fcf085ff3
krzischp <pekrzisch.pek@gmail.com>
2017-06-22 18:35:00 +0200

    Update cordova_intent.mli

--- cordova_intent.mli
+++ cordova_intent.mli
@@ -2,13 +2,11 @@
 type item 
 
 val create_item : uri:string ->
-                  type_:string ->
                   extension:string ->
                   item
 [@@js.builder]
 
 val uri : item -> string
-val type_ : item -> string
 val extension : item -> string
 
 type intent
@@ -16,7 +14,7 @@ type intent
 val create_intent :   action:string ->
                       clipItems:item array ->
                       flags:string ->
-                      type_intent:string ->
+                      type_:string ->
                       component:string ->
                       extras:int ->
                       intent
@@ -25,7 +23,7 @@ val create_intent :   action:string ->
 val action : intent -> string
 val clipItems : intent -> item array
 val flags : intent -> string
-val type_intent : intent -> string
+val type_ : intent -> string
 val component : intent -> string
 val extras : intent -> int
 
